b07380f6cc7eaa71e50bca1b288f9866afdacb3f
Ruben Aguilar <rube.a87@gmail.com>
2022-10-01 00:29:58 -0700

    Adds Stop Button and Validation for the hostname

--- src/index.js
+++ src/index.js
@@ -2,39 +2,21 @@ const path = require('path')
 const express = require('express');
 const spawn = require('child_process').spawn;
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/scan', (req, res) => {
-    const hostToScan = req.query.host
-    if (!hostToScan) return res.send(400);
+    let niktoP = null;
+    const hostToScan = req.query.host;
 
-    // Spawns Nikto as a child process
-    const niktoP = spawn('nikto.pl', ['-h', hostToScan, '-Cgidirs', 'none']);
     // Headers for Server-Sent Events
     res.writeHead(200, {
         'Cache-Control': 'no-cache',
         'Content-Type': 'text/event-stream',
         'Connection': 'keep-alive'
-    });
-    
-    niktoP.stdout.on('data', (data) => {
-        setTimeout(() => {
-            const stdout = data.toString();
-            // console.log(stdout);
-            // \n\n is the delimeter of every event
-            res.write(`data: ${JSON.stringify({ type: "feed", event: stdout })}\n\n`);
-        }, 1000);
-    });
-
-    niktoP.on('close', (code) => {
-        setTimeout(() => {
-            console.log(`Nikto process exited with code ${code}`);
-            res.write(`data: ${JSON.stringify({ type: "close", event: `Scanning host: ${hostToScan} has ended!` })}\n\n`);
-        }, 1000);
     });
 
     req.on('close', () => {
@@ -45,6 +27,31 @@ app.get('/scan', (req, res) => {
             niktoP.kill();
         }
     });
+
+    if (!hostToScan) {
+        setTimeout(() => {
+            res.write(`data: ${JSON.stringify({ type: "error", event: "A valid hostname is required" })}\n\n`);
+        }, 1000);
+    } else {
+        // Spawns Nikto as a child process
+        // Use only to scan non-CGI apps
+        niktoP = spawn('nikto.pl', ['-h', hostToScan, '-Cgidirs', 'none']);
+        niktoP.stdout.on('data', (data) => {
+            setTimeout(() => {
+                const stdout = data.toString();
+                // console.log(stdout);
+                // \n\n is the delimeter of every event
+                res.write(`data: ${JSON.stringify({ type: "feed", event: stdout })}\n\n`);
+            }, 1000);
+        });
+
+        niktoP.on('close', (code) => {
+            setTimeout(() => {
+                console.log(`Nikto process exited with code ${code}`);
+                res.write(`data: ${JSON.stringify({ type: "done", event: `Scanning host: ${hostToScan} has ended!` })}\n\n`);
+            }, 1000);
+        });
+    }
 });
 
 app.listen(PORT, () => {
