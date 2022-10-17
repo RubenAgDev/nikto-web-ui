eb7789e554f649bdbb0e0edc590cb7c67315c660
Ruben Aguilar <rube.a87@gmail.com>
2022-10-17 12:39:33 -0700

    HTML to React

--- components/ScannerOptionsForm.tsx
+++ components/ScannerOptionsForm.tsx
@@ -1,5 +1,5 @@
-import type { ChangeEventHandler, MouseEventHandler } from "react";
-import { ScannerStatus } from "../models";
+import type { ChangeEventHandler, MouseEventHandler } from 'react';
+import { ScannerStatus } from '../models';
 
 interface ScannerOptionsFormProps {
   scannerStatus: ScannerStatus,
@@ -9,17 +9,17 @@ interface ScannerOptionsFormProps {
 
 const ScannerOptionsForm = ({scannerStatus, onChangeHostname, onScanClick}: ScannerOptionsFormProps) => {
   return (
-    <div className="row align-items-center">
-      <div className="col">
-        <label htmlFor="host-input" className="visually-hidden">Enter the hostname to scan:</label>
-        <input type="text" className="form-control" id="host-input" placeholder="https://example.com" onChange={onChangeHostname} />
+    <div className='row align-items-center'>
+      <div className='col'>
+        <label htmlFor='host-input' className='visually-hidden'>Enter the hostname to scan:</label>
+        <input type='text' className='form-control' id='host-input' placeholder='https://example.com' onChange={onChangeHostname} />
       </div>
-      <div className="col p-3">
-        <button type="button" className="btn btn-primary" id="scan-button" onClick={onScanClick}>
+      <div className='col p-3'>
+        <button type='button' className='btn btn-primary' id='scan-button' onClick={onScanClick}>
           { scannerStatus == ScannerStatus.Running &&
-            <span className="spinner-border spinner-border-sm scan-status-indicator" role="status" aria-hidden="true"></span>
+            <span className='spinner-border spinner-border-sm scan-status-indicator' role='status' aria-hidden='true'></span>
           }
-          <span id="scan-button-text">{scannerStatus == ScannerStatus.Running ? "Stop" : "Start"}</span>
+          <span id='scan-button-text'>{scannerStatus == ScannerStatus.Running ? 'Stop' : 'Start'}</span>
         </button>
       </div>
     </div>
--- components/ScannerOutput.tsx
+++ components/ScannerOutput.tsx
@@ -1,4 +1,4 @@
-import { ScanEvent, ScannerStatus } from "../models";
+import { ScanEvent, ScannerStatus } from '../models';
 
 interface ScannerOutputProps {
   scannerStatus: ScannerStatus,
@@ -7,19 +7,19 @@ interface ScannerOutputProps {
 
 const ScannerOutput = ({events, scannerStatus}: ScannerOutputProps) => {
   return (
-    <div className="card">
-      <div className="card-body">
-        <div className="card-text" id="event-list" style={{height: "500px", overflowY: "scroll"}}>
+    <div className='card'>
+      <div className='card-body'>
+        <div className='card-text' id='event-list' style={{height: '500px', overflowY: 'scroll'}}>
           { events.map((item, idx) => (
             <p className={`${item.cssClass} p-2 mb-2 fw-lighter`} key={`scan-event-${idx}`}>
               {item.textContent}
             </p>
           ))}
         </div>
-        <div className="card-text">
+        <div className='card-text'>
           { scannerStatus == ScannerStatus.Running && 
-            <div className="spinner-grow scan-status-indicator text-secondary" role="status">
-              <span className="visually-hidden">Scanning...</span>
+            <div className='spinner-grow scan-status-indicator text-secondary' role='status'>
+              <span className='visually-hidden'>Scanning...</span>
             </div>
           }
         </div>
